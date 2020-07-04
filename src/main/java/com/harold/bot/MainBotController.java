72ef34edee2f1544dd69352e93e625307de13b9f
haroldekb <haroldekb@mail.ru>
2020-07-04 17:40:14 +0500

    init commit

--- src/main/java/com/harold/bot/MainBotController.java
+++ src/main/java/com/harold/bot/MainBotController.java
@@ -1,6 +1,7 @@
 package com.harold.bot;
 
 import com.harold.bot.entity.ConfirmationRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,13 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
 public class MainBotController {
-
+    @Value("${confirmationResponse}")
+    private String confirmationResponse;
 
     //Handle confirmation request for VK Callback API
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public String vkBotConfirmation(@RequestBody ConfirmationRequest request){
         if (request.getType().equals("confirmation") && Integer.parseInt(request.getGroup_id()) == 196557206) {
-            return "c8369b34";
+            return confirmationResponse;
         }
         else return "";
     }
